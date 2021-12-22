deb808f82b91a494527089b964e29cadc5cd5c19
Andereh <ronaldherpc@gmail.com>
2021-12-21 19:43:09 -0800

    Ruffini a medio hacer, intentado hacer una funcion que mida arrays

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+int len(int n)
+{
+    return (int)(sizeof(n)/sizeof(n[0]));
+}
+
+
 int main()
 {
+    int base [] = {5, 20, 15, -20, -20};
+    int restos [sizeof(base)/sizeof(base[0]) - 1];
+
+    int results [sizeof(base)/sizeof(base[0])];
     
-    printf("Hello world!");
+    results[0] = base[0];
+
+    for(int i = 1; i < len(base); i++)
+    {
+        restos[i] = results[i-1];
+    }
+    return 0;
 }
